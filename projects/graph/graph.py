617b1804d535316f117fc927fd820f3d75aa692c
angel-torres <alas4224@gmail.com>
2019-12-10 16:39:51 -0600

    finished add_vertex and add_edge

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -9,18 +9,20 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex_id):
+    def add_vertex(self, vertex):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
-
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
